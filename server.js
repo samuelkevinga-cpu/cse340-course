e64f447b420ab60bd3983c00dd8b5b8e8ffee385
samuelkevinga-cpu <samuelkevinga@gmail.com>
2026-07-03 10:15:31 -0600

    W01 Assignment: Initial Site Creation

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-// Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 
-// Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
@@ -13,27 +11,19 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-/**
-  * Configure Express middleware
-  */
+app.set('view engine', 'ejs');
 
-// Serve static files from the public directory
+app.set('views', path.join(__dirname, 'src/views'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-/**
-  * Routes
-  */
-
-/**
- * Routes
- */
 app.get('/', async (req, res) => {
     const title = 'Home';
     res.render('home', { title });
 });
 
 app.get('/organizations', async (req, res) => {
-    const title = 'Our Partner Organizations';
+    const title = 'Organizations';
     res.render('organizations', { title });
 });
 
@@ -42,11 +32,10 @@ app.get('/projects', async (req, res) => {
     res.render('projects', { title });
 });
 
-// Set EJS as the templating engine
-app.set('view engine', 'ejs');
-
-// Tell Express where to find your templates
-app.set('views', path.join(__dirname, 'src/views'));
+app.get('/categories', async (req, res) => {
+    const title = 'Categories';
+    res.render('categories', { title });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://127.0.0.1:${PORT}`);
