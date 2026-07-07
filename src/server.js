23ad7dc8cfaa30b4a9cfba64a16de0ecb45f6561
samuelkevinga-cpu <samuelkevinga@gmail.com>
2026-07-07 13:21:46 -0600

    Learning activities

---
+++ src/server.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { getAllOrganizations } from './models/organizations.js';
+import { testConnection } from './models/db.js';
+
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/', async (req, res) => {
+    const title = 'Home';
+    res.render('home', { title });
+});
+
+app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    const title = 'Our Partner Organizations';
+
+    res.render('organizations', { title, organizations });
+});
+
+app.get('/projects', async (req, res) => {
+    const title = 'Service Projects';
+    res.render('projects', { title });
+});
+
+app.get('/categories', async (req, res) => {
+    const title = 'Categories';
+    res.render('categories', { title });
+});
+
+app.listen(PORT, async () => {
+    try {
+        await testConnection();
+        console.log(`Server is running at http://127.0.0.1:${PORT}`);
+        console.log(`Environment: ${NODE_ENV}`);
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
+});
